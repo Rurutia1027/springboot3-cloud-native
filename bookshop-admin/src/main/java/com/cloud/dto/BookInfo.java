c611be65f86ab74c67f8d4b68cbca255ae6884ac
Rurutia1027 <rurutia1027@gmail.com>
2025-03-09 00:05:47 +0800

    implement controller for book

---
+++ bookshop-admin/src/main/java/com/cloud/dto/BookInfo.java
@@ -0,0 +1,13 @@
+package com.cloud.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BookInfo {
+    private Long id;
+    private String name;
+}
--- bookshop-admin/src/test/java/com/cloud/web/controller/BookControllerTest.java
+++ bookshop-admin/src/test/java/com/cloud/web/controller/BookControllerTest.java
@@ -1,28 +1,23 @@
 package com.cloud.web.controller;
 
-import com.cloud.web.BaseTest;
 import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+class BookControllerTest {
 
-class BookControllerTest extends BaseTest {
     @Autowired
-    private WebApplicationContext wac;
-
     private MockMvc mockMvc;
-
-    @Before
-    public void setup() {
-        // this will generate a mock MVC context for testing
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-    }
 
     @Test
     public void whenBookQuerySuccess() throws Exception {
