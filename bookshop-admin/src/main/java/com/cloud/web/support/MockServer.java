55daa14942588f6e0d41f1a5fa9784c4de876111
Rurutia1027 <rurutia1027@gmail.com>
2025-03-10 01:51:37 +0800

    Add comments

--- bookshop-admin/src/main/java/com/cloud/web/support/MockServer.java
+++ bookshop-admin/src/main/java/com/cloud/web/support/MockServer.java
@@ -1,10 +1,12 @@
 package com.cloud.web.support;
 
 
-import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
 import com.github.tomakehurst.wiremock.client.WireMock;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
 
+// as long as we can specify the expected return response body
+// we can hand the mock datasets to GPT
 public class MockServer {
     public static void main(String[] args) {
         WireMock.configureFor("127.0.0.1", 8080);
