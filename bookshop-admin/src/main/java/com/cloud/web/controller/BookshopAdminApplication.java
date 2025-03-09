917524ca8e3638b5a66ede50f1433b993b4260d3
Rurutia1027 <rurutia1027@gmail.com>
2025-03-09 23:51:03 +0800

    update codes for filter and interceptor

--- bookshop-admin/src/main/java/com/cloud/web/controller/BookshopAdminApplication.java
+++ bookshop-admin/src/main/java/com/cloud/web/controller/BookshopAdminApplication.java
@@ -2,10 +2,8 @@ package com.cloud.web.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
-@EnableWebMvc
+@SpringBootApplication(scanBasePackages = "com.cloud.web")
 public class BookshopAdminApplication {
 
     public static void main(String[] args) {
---
+++ bookshop-admin/src/main/java/com/cloud/web/interceptor/TimeInterceptor.java
@@ -0,0 +1,37 @@
+package com.cloud.web.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Component
+public class TimeInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println(((HandlerMethod) handler).getBean().getClass().getName());
+        System.out.println(((HandlerMethod) handler).getMethod().getName());
+        request.setAttribute("startTime", new Date().getTime());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("#postHandle");
+        System.out.println("server processing time consume: " + (new Date().getTime() - (Long) request.getAttribute("startTime")) + " ms");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("#afterCompletion");
+        if (Objects.nonNull(ex)) {
+            System.out.println("Exp " + ex.getMessage());
+        }
+        System.out.println("server processing time consume: " + (new Date().getTime() - (Long) request.getAttribute("startTime")) + " ms");
+    }
+}
