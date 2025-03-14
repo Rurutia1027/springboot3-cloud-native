2074d1d827a4adf58ab29cea7fc1c781c17aa641
Rurutia1027 <rurutia1027@gmail.com>
2025-03-14 20:04:50 +0800

    add service layer implmentation and expand more functions in controller layer

--- bookshop-service/src/main/java/com/cloud/bookshop/dubbo/service/BookService.java
+++ bookshop-service/src/main/java/com/cloud/bookshop/dubbo/service/BookService.java
@@ -1,7 +1,19 @@
 package com.cloud.bookshop.dubbo.service;
 
+import com.cloud.bookshop.dubbo.dto.BookCondition;
 import com.cloud.bookshop.dubbo.dto.BookInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookInfo getInfo(Long id);
+
+    Page<BookInfo> query(BookCondition condition, Pageable pageable);
+
+    BookInfo create(BookInfo info);
+
+    BookInfo update(BookInfo info);
+
+    void delete(Long id);
+
 }
